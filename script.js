904602c07fd7a239ca768b14253396b9a0cff29c
theo-githu <111968864+theo-githu@users.noreply.github.com>
2023-09-27 17:40:46 +0100

    popup large screen

--- script.js
+++ script.js
@@ -84,81 +84,21 @@ window.ontouchmove = e => handleOnMove(e.touches[0]);
 
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Check if the user has closed the pop-up before
-  if (localStorage.getItem("popupClosed") !== "true") {
-      // Display the pop-up
-      document.getElementById("popup").style.display = "flex";
-
-      // Add event listener to the close button
-      document.getElementById("closePopup").addEventListener("click", function () {
-          // Close the pop-up
-          document.getElementById("popup").style.display = "none";
-          
-          // Set a flag in localStorage to indicate that the pop-up has been closed
-          localStorage.setItem("popupClosed", "true");
-      });
+  // Check if the popup has been closed before
+  if (!localStorage.getItem("popupClosed")) {
+    // Show the popup if it hasn't been closed
+    document.getElementById("popup").style.display = "block";
   }
+
+  // Handle click on the close icon
+  document.getElementById("closePopup").addEventListener("click", function () {
+    // Hide the popup
+    document.getElementById("popup").style.display = "none";
+
+    // Set a flag in localStorage to indicate that the popup has been closed
+    localStorage.setItem("popupClosed", "true");
+  });
 });
 
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   const images = document.querySelectorAll(".image");
-//   const popup = document.getElementById("popup");
-//   const popupImage = document.getElementById("popup-image");
-//   const popupClose = document.getElementById("popup-close");
-//   const popupPrev = document.getElementById("popup-prev");
-//   const popupNext = document.getElementById("popup-next");
-//   let currentIndex = 0;
-
-//   function openPopup(index) {
-//       currentIndex = index;
-//       popup.style.display = "block";
-//       popupImage.src = images[index].src;
-//   }
-
-//   function closePopup() {
-//       popup.style.display = "none";
-//   }
-
-//   function navigate(direction) {
-//       currentIndex += direction;
-//       if (currentIndex < 0) {
-//           currentIndex = images.length - 1;
-//       }
-//       if (currentIndex >= images.length) {
-//           currentIndex = 0;
-//       }
-//       popupImage.src = images[currentIndex].src;
-//   }
-
-//   images.forEach((image, index) => {
-//       image.addEventListener("click", () => openPopup(index));
-//   });
-
-//   popupClose.addEventListener("click", closePopup);
-//   popupPrev.addEventListener("click", () => navigate(-1));
-//   popupNext.addEventListener("click", () => navigate(1));
-
-//   popupImage.addEventListener("click", () => closePopup());
-
-//   // Close popup when the user clicks outside the image
-//   popup.addEventListener("click", (e) => {
-//       if (e.target === popup) {
-//           closePopup();
-//       }
-//   });
-
-//   // Keyboard navigation
-//   document.addEventListener("keydown", (e) => {
-//       if (popup.style.display === "block") {
-//           if (e.key === "ArrowLeft") {
-//               navigate(-1);
-//           } else if (e.key === "ArrowRight") {
-//               navigate(1);
-//           } else if (e.key === "Escape") {
-//               closePopup();
-//           }
-//       }
-//   });
-// });
 
